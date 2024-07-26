b2540453a4a45a863ccde8df6a549b4dd9c8fbc7
Daniel Berthran <berthlearn@gmail.com>
2024-07-26 10:54:53 +0100

    Fix syntax error at app.run() method

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,4 +15,4 @@ def homepage():
 
 
 if __name__ == '__main__':
-    app.run(host=0.0.0.0, port=5000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
