6348c9b0e72ace9023849e7a747e7f1782d50988
Daniel Berthran <berthlearn@gmail.com>
2024-07-26 12:07:51 +0100

    Define route for displaying a number using a type converter

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''
+A script that starts a Flask web application.
+
+Defines routes for:
+/, /hbnb, /c/<text>, /python/<text> and /number/<n>
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def show_homepage():
+    '''Handles the logic for the home page'''
+    return f"Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    '''Displays the text HBNB'''
+    return f"HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    '''Displays 'C' followed by the value of the text variable'''
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    '''Displays 'Python' followed by the value of the text variable'''
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    '''Displays the value of n only if it is integer'''
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
--- web_flask/project_study/how_to_handle_variables_in_routes.md
+++ web_flask/project_study/how_to_handle_variables_in_routes.md
@@ -19,3 +19,9 @@ def show_user_profile(username):
     # show the user profile for that user
     return f'User {escape(username)}'
 ```
+```py
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+```
