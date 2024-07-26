94d69272fa5bd8a0c56bfa67e22034dd010139c8
Daniel Berthran <berthlearn@gmail.com>
2024-07-26 11:19:56 +0100

    Define route for "c" that handles a variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+A script that starts a Flask web application.
+
+Defines routes for '/', '/hbnb' and '/c/<text>'
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def homepage():
+    '''Handles the logic for the home page'''
+    return f"Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Displays the text HBNB'''
+    return f"HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    '''Displays 'C' followed by the value of the text variable'''
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
