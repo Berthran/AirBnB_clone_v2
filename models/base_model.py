078f8cfa9ad2569c94d30eb6983c7e0374c2eb64
Daniel Berthran <berthlearn@gmail.com>
2024-07-28 05:40:58 +0100

    Add class attributes and modify __init__() and save() methods

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,19 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    # Class attributes mapped to columns in database
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(),nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            print("Ran if block")
-            self.id = Column(String(60), primary_key=True, nullable=False)
-            self.created_at = Column(DateTime, default=datetime.utcnow(),
-                                     nullable=False)
-            self.updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-            """ self.id = str(uuid.uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self) """
+            # storage.new(self)
         else:
             try:
                 kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
@@ -33,6 +34,7 @@ class BaseModel:
                 pass
             self.__dict__.update(kwargs)
 
+
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
@@ -40,7 +42,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        from models.__init__ import storage
         self.updated_at = datetime.now()
         storage.new(self)                         
         storage.save()
