bbd848e955e522a4d3fcfe1481e3f447f99009da
Daniel Berthran <berthlearn@gmail.com>
2024-07-28 05:44:20 +0100

    modify __init__(), all() and delete() methods, and add reload() method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,8 @@
 import os
 import json
 from sqlalchemy import create_engine, text, inspect
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
 
 class DBStorage:
     """This class manages storage of hbnb models using MySQL database"""
@@ -19,14 +20,8 @@ class DBStorage:
         db = os.getenv('HBNB_MYSQL_DB')
         db_env = os.getenv('HBNB_ENV')
 
-        print(db_user, db_pwd, db_host, db, db_env)
-        
         # Create database engine
         self.__engine = create_engine(f'mysql+mysqldb://{db_user}:{db_pwd}@{db_host}/{db}', pool_pre_ping=True)
-
-        # Create session for querying database
-        Session = sessionmaker(bind=self.__engine)
-        self.__session = Session()
 
         # Remove tables if in "test" environment
         if db_env == 'test':
@@ -34,8 +29,7 @@ class DBStorage:
             table_names = inspector.get_table_names()
             for table in table_names:
                 drop_query = text(f"DROP TABLE IF EXISTS {table}")
-                print(f"Fake Dropping table {table}")
-                #self.__session.execute(drop_query)
+                self.__session.execute(drop_query)
 
     def all(self, cls=None):
         dictionary = {}
@@ -45,8 +39,8 @@ class DBStorage:
                 key = f"{cls.__name__}.{obj.id}"
                 dictionary[key] = obj
         else:
-            for subclass in Base.__subclassess__():
-                query_result = self.__session.query(subclass).all
+            for subclass in Base.__subclasses__():
+                query_result = self.__session.query(subclass).all()
                 for obj in query_result:
                     key = f"{subclass.__name__}.{obj.id}"
                     dictionary[key] = obj
@@ -65,4 +59,23 @@ class DBStorage:
 
     def delete(self, obj=None):
         '''Removes an object from the database session'''
+        if obj:
+            self.__session.delete(obj)
+            self.save()
 
+    def reload(self):
+        '''Create all tables in the database'''
+        # Importing subclasses of Base Class
+        from models.city import City
+        from models.user import User
+        from models.state import State
+        from models.place import Place
+        from models.review import Review
+        from models.amenity import Amenity
+
+        # Create tables in the database
+        Base.metadata.create_all(self.__engine)
+
+        # Create session for querying database
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        DBStorage.__session = scoped_session(Session)
