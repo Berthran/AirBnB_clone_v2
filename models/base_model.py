b59723c803e5f8bc10cd53180e0566310f941458
Daniel Berthran <berthlearn@gmail.com>
2024-07-28 15:20:15 +0100

    Set id, created_at and updated_at instance attributes for instantiation with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,9 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
                 del kwargs['__class__']
             except Exception:
-                pass
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
             self.__dict__.update(kwargs)
 
 
