36d5db6d3d6b79d20fc8efc0f6d264b4f83981b2
Daniel Berthran <berthlearn@gmail.com>
2024-07-28 05:46:37 +0100

    correct class attributes definition

--- models/review.py
+++ models/review.py
@@ -9,6 +9,6 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
 
-    place_id = Column(String(60), nullabel=False, foreign_key='places.id')
-    user_id = Column(String(60), nullabel=False, foreign_key='users.id')
-    text = Column(String(1024), nullabel=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
